1e8866bf4f92b1ce5bc340524b03e02c044c3bcc
petersimmalugnt <peter@simmalugnt.se>
2024-04-16 23:02:14 +0200

    modified output.css and tailwind.config.js in .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        "1/1": "1 / 1",
+        "3/2": "3 / 2",
+        "3/4": "3 / 4",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
